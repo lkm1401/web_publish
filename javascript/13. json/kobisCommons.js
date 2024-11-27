cd54a3e5b748cd56e27fb95afd90070572baca4b
lee <lkm1401@naver.com>
2024-11-27 15:16:16 +0900

    commit

--- javascript/13. json/kobisCommons.js
+++ javascript/13. json/kobisCommons.js
@@ -1,4 +1,38 @@
 /**
+ * KMDB 영화 상세 정보
+ */
+export async function kmdbMovieDetail(movieNm, openDt) {
+    const key = '59H5F0U0OFQB3R2261VM';
+    let url = `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/`;
+    url += `search_json2.jsp?collection=kmdb_new2&detail=Y`;
+    url += `&title=${movieNm}&releaseDts=${openDt}`;
+    url += `&ServiceKey=${key}`;
+
+    const result = await fetch(url);
+    const jsonData = await result.json();
+    
+    return jsonData;
+}
+
+
+/**
+ * KMDB 영화 포스터 검색
+ */
+export async function searchMoviePoster(movieNm, openDt) {
+    const key = '59H5F0U0OFQB3R2261VM';
+    let url = `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/`;
+    url += `search_json2.jsp?collection=kmdb_new2&detail=Y`;
+    url += `&title=${movieNm}&releaseDts=${openDt}`;
+    url += `&ServiceKey=${key}`;
+
+    const result = await fetch(url);
+    const jsonData = await result.json();
+    
+    return jsonData.Data[0].Result[0].posters.split("|")[0];
+}
+
+
+/**
  * KOBIS 영화 정보 상세 호출 함수
  */
 export async function kobisMovieDetail(movieCd) {
