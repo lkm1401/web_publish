a2d102033f099ca564180ea39a3ff70914027277
lee <lkm1401@naver.com>
2024-12-06 17:26:14 +0900

    commit 2024-12-06

--- react/basic-app/src/components/cgv_layout/header/HeaderBottomMenuList.jsx
+++ react/basic-app/src/components/cgv_layout/header/HeaderBottomMenuList.jsx
@@ -1,18 +1,33 @@
 import HeaderBottomMenu from './HeaderBottomMenu.jsx';
+import {useState, useEffect} from 'react';
+import { fetchJSON } from '../js/commons.js';
 
 export default function HeaderBottomMenuList() {
-    const names = [
-        { "name": "영화" },   //item => item.name
-        { "name": "극장" },
-        { "name": "예매" },
-        { "name": "스토어" },
-        { "name": "이벤트" },
-        { "name": "혜택" },
-    ];
+    const [names, setNames] = useState([]);
+    
+    useEffect(() => {
+        fetchJSON("/data/cgv_header.json")
+            .then(result => setNames(result.bottomMenuList))
+            .catch(error => console.log(error));
 
+        // fetch("/data/cgv_header.json")
+        //     .then(data => data.json())
+        //     .then(jsonData => setNames(jsonData.bottomMenuList))
+        //     .catch(error => console.log(error));
+    }, []);
+
+    // const menuItems = [];
+    // names && names.forEach(item => {
+    //     menuItems.push (
+    //         <li key={item.name}>
+    //             <HeaderBottomMenu name={item.name} />
+    //         </li>
+    //     );
+    // });
+    
     return (
         <ul class="flex-container">
-            {names.map(item => 
+            {names && names.map(item => 
                 <li>
                     <HeaderBottomMenu name={item.name} />
                 </li>           
