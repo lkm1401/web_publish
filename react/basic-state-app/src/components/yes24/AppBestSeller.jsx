cecc63668e9f444e2797f9400055c0c534a0e54e
lee <lkm1401@naver.com>
2024-12-10 17:13:09 +0900

    commit 2024-12-10

---
+++ react/basic-state-app/src/components/yes24/AppBestSeller.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BestBook from './BestBook.jsx';
+import './BestBook.css';
+
+export default function AppBestSeller() {
+
+    return (
+        <div>
+            <BestBook />
+        </div>
+    );
+}
+
